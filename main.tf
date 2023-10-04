67fa5bdea9d9ff9ab9c5395b6dc47945e61ff951
veeranki2014 <veeranki2014@gmail.com>
2023-10-04 13:48:00 -0500

    terraform rabbitmq module

--- main.tf
+++ main.tf
@@ -4,6 +4,69 @@ resource "aws_elasticache_subnet_group" "main" {
   tags              = merge ({ Name = "${var.component}-${var.env}" }, var.tags )
 }
 
+# Security Groups
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+##Redis Cluster Mode Disabled
+#resource "aws_elasticache_cluster" "main" {
+#  cluster_id           = "${var.component}-${var.env}"
+#  engine               = var.engine
+#  node_type            = var.node_type
+#  num_cache_nodes      = var.num_cache_nodes
+#  parameter_group_name = var.parameter_group_name
+#  engine_version       = var.engine_version
+#  port                 = var.port
+#  subnet_group_name    = aws_elasticache_subnet_group.main.name
+#  security_group_ids   = [ aws_security_group.main.id ]
+#}
+
+##Redis Cluster mode Enabled
+resource "aws_elasticache_replication_group" "main" {
+  replication_group_id       = "${var.component}-${var.env}"
+  description                = "${var.component}-${var.env}"
+  node_type                  = var.node_type
+  port                       = var.port
+  #parameter_group_name       = "default.redis3.2.cluster.on"
+  automatic_failover_enabled = true
+
+  num_node_groups           = var.num_node_groups
+  replicas_per_node_group   = var.replicas_per_node_group
+  subnet_group_name         = aws_elasticache_subnet_group.main.name
+  security_group_ids        = [ aws_security_group.main.id]
+  parameter_group_name      = "default.redis6.x.cluster.on"
+  kms_key_id                = var.kms_key_arn
+  engine                    = var.engine
+  engine_version            = var.engine_version
+  #storage_encrypted         = true
+  #transit_encryption_enabled = true
+  at_rest_encryption_enabled = true
+
+}
+
+
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "tags" {}
 variable "sg_subnet_cidr" {}
 variable "kms_key_arn" {}
 variable "vpc_id" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "parameter_group_name" {}
+variable "engine_version" {}
+variable "port" {
+  default = 6379
+}
+variable "replicas_per_node_group" {}
+variable "num_node_groups" {}
